d673a7218c15bfcca01681882e9331e3a49d204e
goomon <jaegoo.jho@gmail.com>
2023-10-23 11:36:40 +0900

    Eureka server 실행하기

--- src/main/java/com/github/goomon/cloud/CloudApplication.java
+++ src/main/java/com/github/goomon/cloud/CloudApplication.java
@@ -3,9 +3,12 @@ package com.github.goomon.cloud;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CloudApplication {
 
 	public static void main(String[] args) {
