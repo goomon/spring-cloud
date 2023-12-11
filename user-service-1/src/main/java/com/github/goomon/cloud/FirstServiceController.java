54d2f08cdab4899aa150fa967df1c00752ab5e21
goomon <jaegoo.jho@gmail.com>
2023-12-11 22:49:38 +0900

    FilterConfig (Java 코드로 라우팅)

--- user-service-1/src/main/java/com/github/goomon/cloud/FirstServiceController.java
+++ user-service-1/src/main/java/com/github/goomon/cloud/FirstServiceController.java
@@ -1,14 +1,21 @@
 package com.github.goomon.cloud;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
 public class FirstServiceController {
-    @GetMapping("/first-service/welcome")
+    @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to first service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        System.out.println(header);
+        return "Hello World in first service.";
     }
 }
--- user-service-2/src/main/java/com.github.goomon.cloud/SecondServiceController.java
+++ user-service-2/src/main/java/com.github.goomon.cloud/SecondServiceController.java
@@ -1,14 +1,21 @@
 package com.github.goomon.cloud;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
 public class SecondServiceController {
-    @GetMapping("/second-service/welcome")
+    @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to second service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        System.out.println(header);
+        return "Hello World in second service.";
     }
 }
