c123138fb4c6946104a213ccb7bf844682e158a5
goomon <jaegoo.jho@gmail.com>
2023-12-11 23:16:38 +0900

    CustomFilter

--- apigw/src/main/java/com/github/goomon/cloud/config/FilterConfig.java
+++ apigw/src/main/java/com/github/goomon/cloud/config/FilterConfig.java
@@ -1,5 +1,6 @@
 package com.github.goomon.cloud.config;
 
+import com.github.goomon.cloud.filter.CustomFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,12 +8,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterConfig {
+
+    final CustomFilter customFilter;
+
+    public FilterConfig(CustomFilter customFilter) {
+        this.customFilter = customFilter;
+    }
+
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r
                         .path("/first-service/**")
                         .filters(f -> f
+                                .filter(customFilter.apply(new CustomFilter.Config()))
                                 .addRequestHeader("first-request", "first-request-header")
                                 .addResponseHeader("first-response", "first-response-header"))
                         .uri("http://localhost:8081"))
